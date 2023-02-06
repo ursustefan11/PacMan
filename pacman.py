41d3141d0dd8fd9cb22aea21f6863d977a525b08
Stefan Ursu <ursustefan11@gmail.com>
2023-02-06 11:52:46 +0000

    Player can eat point and gain score

--- pacman.py
+++ pacman.py
@@ -3,7 +3,9 @@ from board import boards
 import pygame
 import math
 
+
 pygame.init()
+
 
 # Game Settings
 WIDTH = 900
@@ -20,17 +22,21 @@ for i in range(1, 5):
 
 player_x = 440
 player_y = 663
-direction = 0
-counter = 0
-flicker = False
+player_speed = 2
+
 
 # Set the variables
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
 timer = pygame.time.Clock()
 font = pygame.font.Font('assets/OpenSansRegular.ttf', 20)
 PI = math.pi
-# R, L, U, D
-turns_allowed = [False, False, False, False]
+turns_allowed = [False, False, False, False]  # R, L, U, D
+direction_command = 0
+direction = 0
+counter = 0
+flicker = False
+score = 0
+
 
 # Draw the board. 32x32, Tile-based.
 def draw_board():
@@ -67,6 +73,7 @@ def draw_board():
                                  (0.5 * num1)), (j * num2 + num2, i * num1 + (0.5 * num1)), 3)
 
 
+# Draw the player. Rotate according to the direction.
 def draw_player():
     # 0-RIGHT, 1-LEFT, 2-UP, 3-DOWN
     if direction == 0:
@@ -82,6 +89,7 @@ def draw_player():
             player_images[counter // 5], -90), (player_x, player_y))
 
 
+# Check if next position is available.
 def check_position(center_x, center_y):
     turns = [False, False, False, False]
     num1 = (HEIGHT - 50) // 32
@@ -113,6 +121,7 @@ def check_position(center_x, center_y):
                     turns[3] = True
                 if level[(center_y - num3) // num1][center_x // num2] < 3:
                     turns[2] = True
+            
             if 12 <= center_y % num1 <= 18:
                 if level[center_y // num1][(center_x - num2)// num2] < 3:
                     turns[1] = True
@@ -125,6 +134,7 @@ def check_position(center_x, center_y):
                     turns[3] = True
                 if level[(center_y - num1) // num1][center_x // num2] < 3:
                     turns[2] = True
+            
             if 12 <= center_y % num1 <= 18:
                 if level[center_y // num1][(center_x - num3) // num2] < 3:
                     turns[1] = True
@@ -136,6 +146,38 @@ def check_position(center_x, center_y):
     
     return turns
 
+
+# Move player if next position is available and key is pressed or direction is set.
+def move_player(move_x, move_y):
+    # R, L, U, D
+    if direction == 0 and turns_allowed[0]:
+        move_x += player_speed
+    elif direction == 1 and turns_allowed[1]:
+        move_x -= player_speed
+
+    if direction == 2 and turns_allowed[2]:
+        move_y -= player_speed
+    elif direction == 3 and turns_allowed[3]:
+        move_y += player_speed
+    return move_x, move_y
+
+def check_collisions(scor):
+    num1 = (HEIGHT - 50) // 32
+    num2 = WIDTH // 30
+    if 0 < player_x < 870:
+        if level[center_y // num1][center_x // num2] == 1:
+            level[center_y // num1][center_x // num2] = 0
+            scor += 10
+        if level[center_y // num1][center_x // num2] == 2:
+            level[center_y // num1][center_x // num2] = 0
+            scor += 50
+    return scor
+
+
+def draw_misc():
+    score_text = font.render(f'Score: {score}', True, 'white')
+    screen.blit(score_text, (10, 920))
+
 run = True
 while run:
     timer.tick(fps)
@@ -143,6 +185,7 @@ while run:
         counter += 1
         if counter > 3:
             flicker = False
+    
     else:
         counter = 0
         flicker = True
@@ -150,24 +193,52 @@ while run:
     screen.fill('black')
     draw_board()
     draw_player()
+    draw_misc()
     center_x = player_x + 23
     center_y = player_y + 24
     turns_allowed = check_position(center_x, center_y)
+    player_x, player_y = move_player(player_x, player_y)
+    score = check_collisions(score)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
         # If a key is pressed
         if event.type == pygame.KEYDOWN:
-            #If a specific key is pressed
+            # If a specific key is pressed
             if event.key == pygame.K_RIGHT:
-                direction = 0
+                direction_command = 0
             if event.key == pygame.K_LEFT:
-                direction = 1
+                direction_command = 1
             if event.key == pygame.K_UP:
-                direction = 2
+                direction_command = 2
             if event.key == pygame.K_DOWN:
-                direction = 3
+                direction_command = 3
+        
+        # If a key a key is released
+        if event.type == pygame.KEYUP:
+            # If key is released keep going in the direction
+            if event.key == pygame.K_RIGHT and direction_command == 0:
+                direction_command = direction
+            if event.key == pygame.K_LEFT and direction_command == 1:
+                direction_command = direction
+            if event.key == pygame.K_UP and direction_command == 2:
+                direction_command = direction
+            if event.key == pygame.K_DOWN and direction_command == 3:
+                direction_command = direction
+        # If the for loop and the next if/elif statement is inside the `for event in pygame.event` loop, you'll have to spam the direction keys in order to change direction when is possible.
+        # If you outdent the for loop and if/elif statement, the player will change direction as soon as there is a position available.
+        # I think that the way that it works right now makes this more engaging.
+        for i in range(4):
+            # If moving in direction and next pos is available then keep moving
+            if direction_command == i and turns_allowed[i]:
+                direction = i
+
+        if player_x > 900:
+            player_x = -47
+        elif player_x < -50:
+            player_x = 897
 
     pygame.display.flip() # Updates to the visuals
 pygame.quit()
